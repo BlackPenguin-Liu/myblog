20bb8f47230dfb503a519118218eb233833f9832
BlackPenguin-Liu <BlackPenguin-Liu@users.noreply.github.com>
2026-02-08 14:06:55 +0000

    deploy: b2358da88412891f373bb6822997d3af77a8b1b4

---
+++ docs__tech__Android__ICMP.md.97da9ea0.async.js
@@ -0,0 +1,150 @@
+"use strict";(self.webpackChunkmyblog=self.webpackChunkmyblog||[]).push([[575],{64751:function(s,a,t){t.r(a);var l=t(72269),x=t(93359),I=t(61788),p=t(19977),_=t(25809),i=t(90978),d=t(96057),m=t(83213),r=t(53683),c=t(80936),o=t(67294),n=t(40347),e=t(85893);function u(){return(0,e.jsx)(r.dY,{children:(0,e.jsx)(o.Suspense,{fallback:(0,e.jsx)(c.Z,{}),children:(0,e.jsx)(e.Fragment,{children:(0,e.jsxs)("div",{className:"markdown",children:[(0,e.jsxs)("h1",{id:"icmp",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#icmp",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"ICMP"]}),(0,e.jsx)("p",{children:n.texts[0].value}),(0,e.jsxs)("h2",{id:"1-\u7279\u6027\u8BF4\u660E",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#1-\u7279\u6027\u8BF4\u660E",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"1 \u7279\u6027\u8BF4\u660E"]}),(0,e.jsxs)("ul",{children:[(0,e.jsx)("li",{children:n.texts[1].value}),(0,e.jsx)("li",{children:n.texts[2].value}),(0,e.jsx)("li",{children:n.texts[3].value}),(0,e.jsx)("li",{children:n.texts[4].value}),(0,e.jsx)("li",{children:n.texts[5].value})]}),(0,e.jsxs)("h2",{id:"2-\u4E3B\u8981\u7528\u9014",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#2-\u4E3B\u8981\u7528\u9014",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"2 \u4E3B\u8981\u7528\u9014"]}),(0,e.jsxs)("ol",{children:[(0,e.jsxs)("li",{children:[(0,e.jsx)("p",{children:n.texts[6].value}),(0,e.jsx)("p",{children:n.texts[7].value}),(0,e.jsx)("p",{children:n.texts[8].value})]}),(0,e.jsxs)("li",{children:[(0,e.jsx)("p",{children:n.texts[9].value}),(0,e.jsx)("p",{children:n.texts[10].value})]}),(0,e.jsxs)("li",{children:[(0,e.jsx)("p",{children:n.texts[11].value}),(0,e.jsx)("p",{children:n.texts[12].value}),(0,e.jsx)("p",{children:n.texts[13].value}),(0,e.jsx)("p",{children:n.texts[14].value})]})]}),(0,e.jsxs)("h3",{id:"21-ping-\u5DE5\u4F5C\u6D41\u7A0B",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#21-ping-\u5DE5\u4F5C\u6D41\u7A0B",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"2.1 Ping \u5DE5\u4F5C\u6D41\u7A0B"]}),(0,e.jsxs)(d.Z,{children:[(0,e.jsx)("thead",{children:(0,e.jsxs)("tr",{children:[(0,e.jsx)("th",{children:n.texts[15].value}),(0,e.jsx)("th",{children:n.texts[16].value}),(0,e.jsx)("th",{children:n.texts[17].value})]})}),(0,e.jsxs)("tbody",{children:[(0,e.jsxs)("tr",{children:[(0,e.jsx)("td",{children:n.texts[18].value}),(0,e.jsx)("td",{children:n.texts[19].value}),(0,e.jsx)("td",{children:n.texts[20].value})]}),(0,e.jsxs)("tr",{children:[(0,e.jsx)("td",{children:n.texts[21].value}),(0,e.jsx)("td",{children:n.texts[22].value}),(0,e.jsx)("td",{children:(0,e.jsx)("code",{children:n.texts[23].value})})]}),(0,e.jsxs)("tr",{children:[(0,e.jsx)("td",{children:n.texts[24].value}),(0,e.jsx)("td",{children:n.texts[25].value}),(0,e.jsx)("td",{children:n.texts[26].value})]}),(0,e.jsxs)("tr",{children:[(0,e.jsx)("td",{children:n.texts[27].value}),(0,e.jsx)("td",{children:n.texts[28].value}),(0,e.jsxs)("td",{children:[(0,e.jsx)("code",{children:n.texts[29].value}),n.texts[30].value]})]}),(0,e.jsxs)("tr",{children:[(0,e.jsx)("td",{children:n.texts[31].value}),(0,e.jsx)("td",{children:n.texts[32].value}),(0,e.jsx)("td",{children:n.texts[33].value})]})]})]}),(0,e.jsxs)("h3",{id:"22-\u4F7F\u7528icmp-\u5B9E\u73B0-ping-\u7684\u4F18\u70B9",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#22-\u4F7F\u7528icmp-\u5B9E\u73B0-ping-\u7684\u4F18\u70B9",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"2.2 \u4F7F\u7528ICMP \u5B9E\u73B0 ping \u7684\u4F18\u70B9"]}),(0,e.jsxs)("ul",{children:[(0,e.jsx)("li",{children:n.texts[34].value}),(0,e.jsx)("li",{children:n.texts[35].value}),(0,e.jsx)("li",{children:n.texts[36].value}),(0,e.jsx)("li",{children:n.texts[37].value}),(0,e.jsx)("li",{children:n.texts[38].value})]}),(0,e.jsxs)("h3",{id:"23-ping-\u4EE3\u7801",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#23-ping-\u4EE3\u7801",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"2.3 Ping \u4EE3\u7801"]}),(0,e.jsx)(i.Z,{lang:"js",children:n.texts[39].value}),(0,e.jsxs)("h2",{id:"3-icmp\u5305\u7ED3\u6784",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#3-icmp\u5305\u7ED3\u6784",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"3 ICMP\u5305\u7ED3\u6784"]}),(0,e.jsxs)(d.Z,{children:[(0,e.jsx)("thead",{children:(0,e.jsxs)("tr",{children:[(0,e.jsx)("th",{children:n.texts[40].value}),(0,e.jsx)("th",{children:n.texts[41].value})]})}),(0,e.jsxs)("tbody",{children:[(0,e.jsxs)("tr",{children:[(0,e.jsx)("td",{children:n.texts[42].value}),(0,e.jsx)("td",{children:n.texts[43].value})]}),(0,e.jsxs)("tr",{children:[(0,e.jsx)("td",{children:n.texts[44].value}),(0,e.jsx)("td",{children:n.texts[45].value})]}),(0,e.jsxs)("tr",{children:[(0,e.jsx)("td",{children:n.texts[46].value}),(0,e.jsx)("td",{children:n.texts[47].value})]}),(0,e.jsxs)("tr",{children:[(0,e.jsx)("td",{children:n.texts[48].value}),(0,e.jsx)("td",{children:n.texts[49].value})]}),(0,e.jsxs)("tr",{children:[(0,e.jsx)("td",{children:n.texts[50].value}),(0,e.jsx)("td",{children:n.texts[51].value})]})]})]})]})})})})}a.default=u},40347:function(s,a,t){t.r(a),t.d(a,{texts:function(){return l}});const l=[{value:"ICMP \u5168\u79F0 Internet Control Message Protocol\uFF08\u4E92\u8054\u7F51\u63A7\u5236\u6D88\u606F\u534F\u8BAE\uFF09",paraId:0,tocIndex:0},{value:"OSI \u5C42\u7EA7\uFF1A\u7B2C 3 \u5C42\uFF08\u7F51\u7EDC\u5C42\uFF09\uFF0C\u4E0E IP \u534F\u8BAE\u540C\u7EA7",paraId:1,tocIndex:1},{value:"\u529F\u80FD\uFF1A\u7528\u4E8E\u53D1\u9001\u9519\u8BEF\u62A5\u544A\u548C\u8BCA\u65AD\u4FE1\u606F",paraId:1,tocIndex:1},{value:"\u65E0\u8FDE\u63A5\uFF1A\u4E0D\u5EFA\u7ACB\u8FDE\u63A5\uFF0C\u76F4\u63A5\u53D1\u9001\u6D88\u606F",paraId:1,tocIndex:1},{value:"\u4E0D\u53EF\u9760\uFF1A\u6CA1\u6709\u9519\u8BEF\u6062\u590D\u673A\u5236\uFF08best-effort\uFF09",paraId:1,tocIndex:1},{value:"\u7528\u9014\uFF1A\u7F51\u7EDC\u8BCA\u65AD\u3001\u9519\u8BEF\u62A5\u544A\u3001\u7F51\u7EDC\u72B6\u6001\u76D1\u6D4B",paraId:1,tocIndex:1},{value:"Ping\uFF08Echo Request/Reply\uFF09\uFF1A\u6700\u5E38\u89C1\u7684 ICMP \u5E94\u7528\uFF0C\u7528\u6765\u6D4B\u8BD5\u7F51\u7EDC\u8FDE\u63A5\u662F\u5426\u6B63\u5E38\uFF1A",paraId:2,tocIndex:2},{value:"\u53D1\u9001\u7AEF \u2192 ICMP Echo Request \u2192 \u76EE\u6807\u4E3B\u673A",paraId:3,tocIndex:2},{value:"\u53D1\u9001\u7AEF \u2190 ICMP Echo Reply \u2190 \u76EE\u6807\u4E3B\u673A",paraId:4,tocIndex:2},{value:"Traceroute\uFF08\u8FFD\u8E2A\u8DEF\u7531\uFF09\uFF1A\u901A\u8FC7\u53D1\u9001\u5E26\u6709\u9012\u589E TTL\uFF08\u751F\u5B58\u65F6\u95F4\uFF09\u503C\u7684 ICMP \u5305\uFF0C\u663E\u793A\u6570\u636E\u5305\u4ECE\u6E90\u5230\u76EE\u6807\u7684\u8DEF\u7531\u8DEF\u5F84\uFF1A",paraId:5,tocIndex:2},{value:"Host A \u2192 Router 1 \u2192 Router 2 \u2192 Router 3 \u2192 Host B",paraId:6,tocIndex:2},{value:"\u9519\u8BEF\u62A5\u544A\uFF1A\u5F53\u8DEF\u7531\u5668\u6216\u4E3B\u673A\u9047\u5230\u95EE\u9898\u65F6\uFF0C\u53D1\u9001 ICMP \u9519\u8BEF\u6D88\u606F\u901A\u77E5\u6E90\u4E3B\u673A\uFF0C\u4F8B\u5982\uFF1A",paraId:7,tocIndex:2},{value:"\u76EE\u6807\u4E0D\u53EF\u8FBE\uFF08Destination Unreachable\uFF09",paraId:8,tocIndex:2},{value:"\u8D85\u65F6\uFF08Time Exceeded\uFF09",paraId:9,tocIndex:2},{value:"\u53C2\u6570\u95EE\u9898\uFF08Parameter Problem\uFF09",paraId:10,tocIndex:2},{value:"\u5E8F\u53F7",paraId:11,tocIndex:3},{value:"\u6B65\u9AA4",paraId:11,tocIndex:3},{value:"\u8BE6\u89E3",paraId:11,tocIndex:3},{value:"1",paraId:11,tocIndex:3},{value:"\u6784\u9020 ICMP Echo Request \u5305",paraId:11,tocIndex:3},{value:"Type: 8 (IPv4) or 128 (IPv6)\uFF1BSequence Number: \u5E8F\u5217\u53F7\uFF1BPayload: \u53EF\u9009\u6570\u636E",paraId:11,tocIndex:3},{value:"2",paraId:11,tocIndex:3},{value:"\u901A\u8FC7 Socket \u53D1\u9001 ICMP \u5305",paraId:11,tocIndex:3},{value:"Os.write(fileDescriptor, packet)",paraId:11,tocIndex:3},{value:"3",paraId:11,tocIndex:3},{value:"\u76EE\u6807\u4E3B\u673A\u6536\u5230\u8BF7\u6C42",paraId:11,tocIndex:3},{value:"\u81EA\u52A8\u751F\u6210 Echo Reply \u54CD\u5E94\uFF1BType: 0 (IPv4) or 129 (IPv6)",paraId:11,tocIndex:3},{value:"4",paraId:11,tocIndex:3},{value:"\u539F\u4E3B\u673A\u63A5\u6536\u54CD\u5E94",paraId:11,tocIndex:3},{value:"Os.read(fileDescriptor, reply)",paraId:11,tocIndex:3},{value:"\uFF0C\u9A8C\u8BC1\u5E8F\u5217\u53F7\u548C\u5185\u5BB9",paraId:11,tocIndex:3},{value:"5",paraId:11,tocIndex:3},{value:"\u8BA1\u7B97\u5F80\u8FD4\u65F6\u95F4 (RTT)",paraId:11,tocIndex:3},{value:"RTT = \u54CD\u5E94\u65F6\u95F4 - \u53D1\u9001\u65F6\u95F4",paraId:11,tocIndex:3},{value:"\u8F7B\u91CF\u7EA7\uFF1AICMP \u5305\u5F88\u5C0F\uFF08\u901A\u5E38 8 \u5B57\u8282\u8D77\uFF09\uFF0C\u7F51\u7EDC\u5F00\u9500\u5C0F",paraId:12,tocIndex:4},{value:"\u5FEB\u901F\u53CD\u5E94\uFF1A\u6CA1\u6709\u8FDE\u63A5\u5EFA\u7ACB\u8FC7\u7A0B\uFF0C\u7ACB\u5373\u8FD4\u56DE\u7ED3\u679C",paraId:12,tocIndex:4},{value:"\u7F51\u7EDC\u8BCA\u65AD\uFF1A\u53EF\u4EE5\u68C0\u6D4B\u7F51\u7EDC\u8FDE\u63A5\u72B6\u6001\u3001\u5EF6\u8FDF\u3001\u4E22\u5305\u7387",paraId:12,tocIndex:4},{value:"\u8DE8\u5E73\u53F0\uFF1A\u51E0\u4E4E\u6240\u6709\u7F51\u7EDC\u8BBE\u5907\u90FD\u652F\u6301 ICMP",paraId:12,tocIndex:4},{value:"\u4E0D\u4F9D\u8D56\u5E94\u7528\u5C42\uFF1A\u5373\u4F7F\u76EE\u6807\u4E3B\u673A\u6CA1\u6709\u8FD0\u884C\u7279\u5B9A\u670D\u52A1\u4E5F\u80FD\u54CD\u5E94",paraId:12,tocIndex:4},{value:`public class IcmpCheck implements Runnable, Closeable {
+    public static final int ICMPV4_ECHO_REQUEST_TYPE = 8;
+    public static final int ICMPV6_ECHO_REQUEST_TYPE = 128;
+    private static final int TIMEOUT_SEND = 100;
+    private static final int TIMEOUT_RECV = 300;
+    private static final int PACKET_BUFSIZE = 512;
+
+    protected final InetAddress mTarget;
+    protected final int mAddressFamily;
+    protected FileDescriptor mFileDescriptor;
+    protected SocketAddress mSocketAddress;
+
+    private final int mProtocol;
+    private final int mIcmpType;
+    private final long mTimeout;
+    private final int mPingCount;
+
+    protected IcmpCheck(InetAddress target, long timeout, int pingCount) {
+        if (target instanceof Inet6Address) {
+            mTarget = target;
+            mAddressFamily = OsConstants.AF_INET6;
+        } else {
+            mTarget = target;
+            mAddressFamily = OsConstants.AF_INET;
+        }
+
+        this.mTimeout = timeout;
+        this.mPingCount = pingCount;
+        if (mAddressFamily == AF_INET6) {
+            mProtocol = IPPROTO_ICMPV6;
+            mIcmpType = ICMPV6_ECHO_REQUEST_TYPE;
+        } else {
+            mProtocol = IPPROTO_ICMP;
+            mIcmpType = ICMPV4_ECHO_REQUEST_TYPE;
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            setupSocket(mProtocol, TIMEOUT_SEND, TIMEOUT_RECV);
+            doPing();
+        } catch (Exception e) {
+            DxLog.e(TAG, "pingError:" + e.getMessage());
+        } finally {
+            close();
+        }
+    }
+
+// writeTimeout\u53EF\u80FD\uFF1A1. \u7F51\u7EDC\u975E\u5E38\u62E5\u5835\uFF1B2. Socket \u7F13\u51B2\u533A\u6EE1\uFF1B3. \u7CFB\u7EDF\u8D44\u6E90\u4E0D\u8DB3
+// readTimeout\u53EF\u80FD\uFF1A1. \u76EE\u6807\u4E3B\u673A\u65E0\u54CD\u5E94\uFF08\u5B95\u673A\u3001\u79BB\u7EBF\uFF09\uFF1B2. \u7F51\u7EDC\u8FDE\u63A5\u4E2D\u65AD\uFF1B3. \u4E22\u5305\u7387\u8FC7\u9AD8\uFF1B4. \u8DEF\u7531\u5668\u6545\u969C
+    protected void setupSocket(int protocol, long writeTimeout, long readTimeout)
+            throws ErrnoException, IOException {
+        mFileDescriptor = Os.socket(mAddressFamily, OsConstants.SOCK_DGRAM, protocol);
+        if (Build.VERSION.SDK_INT >= 29) {
+            // Setting SNDTIMEO is purely for defensive purposes.
+            Os.setsockoptTimeval(mFileDescriptor,
+                    OsConstants.SOL_SOCKET, OsConstants.SO_SNDTIMEO, StructTimeval.fromMillis(writeTimeout));
+//\u5355\u6B21read\u7684\u8D85\u65F6\u65F6\u95F4
+            Os.setsockoptTimeval(mFileDescriptor,
+                    OsConstants.SOL_SOCKET, OsConstants.SO_RCVTIMEO, StructTimeval.fromMillis(readTimeout));
+        }
+        Os.connect(mFileDescriptor, mTarget, 0);
+        mSocketAddress = Os.getsockname(mFileDescriptor);
+    }
+
+    private void doPing() {
+        // Build a trivial ICMP packet.// ICMP header
+        final byte[] icmpPacket = {(byte) mIcmpType, 0, 0, 0, 0, 0, 0, 0};
+
+        int sequence = 0;
+        while (sequence < mPingCount) {
+            sequence++;
+            icmpPacket[icmpPacket.length - 1] = (byte) sequence;
+            mMeasurement.sequenceStart();
+            try {
+                //\u53D1\u9001icmp\u5305
+                Os.write(mFileDescriptor, icmpPacket, 0, icmpPacket.length);
+                //\u63A5\u6536icmp\u5305
+                checkEcho(sequence);
+            } catch (ErrnoException | InterruptedIOException ignore) {
+                mMeasurement.sequenceError();
+            } finally {
+                delay(10);
+            }
+        }
+    }
+
+    private void checkEcho(int sequence) {
+        long startTime = SystemClock.elapsedRealtime();
+        boolean received = false;
+// \u53EF\u80FD\u591A\u6B21\u5FAA\u73AF\uFF1A1.\u4E3B\u673A\u53EF\u80FD\u53D1\u9001\u591A\u4E2AReply\uFF0C\u53EF\u80FD\u5E8F\u5217\u53F7\u4E0D\u5BF9\uFF0C\u9700\u8981\u9A8C\u8BC1icmp\u5E8F\u5217\u53F7\uFF1B2 \u7F51\u7EDC\u6162\u6216\u65E0\u54CD\u5E94\u3002
+// \u5355\u6B21\u5FAA\u73AFread\u7684\u8D85\u65F6\u65F6\u95F4\u4E3ATIMEOUT_RECV\uFF0C\u53EF\u4EE5\u9632\u6B62read\u6C38\u4E45\u963B\u585E\u3002\u6574\u4F53\u5FAA\u73AF\u7684\u8D85\u65F6\u65F6\u95F4\u4E3AmTimeout\u3002\u53EF\u4EE5\u9632\u6B62\u65E0\u9650\u5236\u7684\u4E00\u76F4\u7B49\u5F85\u3002
+// \u6240\u4EE5\u9700\u8981\u4E24\u4E2A\u65F6\u95F4
+        while (SystemClock.elapsedRealtime() - startTime < mTimeout) {
+            try {
+                ByteBuffer reply = ByteBuffer.allocate(PACKET_BUFSIZE);
+                Os.read(mFileDescriptor, reply);
+                //\u68C0\u67E5icmp\u5305\u5E8F\u53F7
+                if (isValidIcmpReply(reply.array(), sequence)) {
+                    mMeasurement.sequenceSuccess();
+                    received = true;
+                    break;
+                }
+            } catch (ErrnoException | InterruptedIOException ignore) {
+                //read failed
+            }
+        }
+        ///\u8D85\u65F6\u672A\u63A5\u6536\u5230\u6B63\u786E\u54CD\u5E94
+        if (!received) {
+            mMeasurement.sequenceError();
+        }
+    }
+
+    private boolean isValidIcmpReply(byte[] packet, int expectedSequence) {
+        if (mProtocol == IPPROTO_ICMPV6) {
+            return packet[0] == -127 &&               // Type: Echo Reply
+                    packet[1] == 0 &&               // Code: 0
+                    (packet[7] & 0xFF) == expectedSequence; // \u5E8F\u5217\u53F7\u5339\u914D
+        } else {
+            return packet[0] == 0 &&               // Type: Echo Reply
+                    packet[1] == 0 &&               // Code: 0
+                    (packet[7] & 0xFF) == expectedSequence; // \u5E8F\u5217\u53F7\u5339\u914D
+        }
+    }
+
+    private final Object lock = new Object();
+
+    private void delay(long delay) {
+        synchronized (lock) {
+            try {
+                lock.wait(delay);
+            } catch (InterruptedException ignore) {
+            }
+        }
+    }
+
+    @Override
+    public void close() {
+        if (mFileDescriptor != null && mFileDescriptor.valid()) {
+            try {
+                Os.close(mFileDescriptor);
+            } catch (Exception ignore) {
+            }
+        }
+    }
+}
+
+
+`,paraId:13,tocIndex:5},{value:"\u5185\u5BB9",paraId:14,tocIndex:6},{value:"\u542B\u4E49",paraId:14,tocIndex:6},{value:"Type (8 bits)",paraId:14,tocIndex:6},{value:"0: Echo Reply, 8: Echo Request",paraId:14,tocIndex:6},{value:"Code (8 bits)",paraId:14,tocIndex:6},{value:"\u7EC6\u5206\u7C7B\u578B\uFF0C\u901A\u5E38\u4E3A 0",paraId:14,tocIndex:6},{value:"Checksum (16 bits)",paraId:14,tocIndex:6},{value:"\u7528\u4E8E\u9519\u8BEF\u68C0\u6D4B",paraId:14,tocIndex:6},{value:"Rest of Header (32 bits)",paraId:14,tocIndex:6},{value:"Identifier, Sequence Number",paraId:14,tocIndex:6},{value:"Data (\u53EF\u9009)",paraId:14,tocIndex:6},{value:"Payload",paraId:14,tocIndex:6}]}}]);
